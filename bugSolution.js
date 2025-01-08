0fb0a9abc97a55a361ee7430f79f118b6c7be52c
BugHunter-X <askzenai@gmail.com>
2025-01-08 20:52:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,16 @@
+```javascript
+// pages/index.js
+
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <Link href='/about'>
+        <a>Go to About</a>
+      </Link>
+    </div>
+  );
+}
+```
